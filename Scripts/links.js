5f54f6f4a2e9add998df08c6ad9dbb5c2fe07aae
ZackSalah <ms24@pdx.edu>
2019-08-17 16:15:09 +0300

    final product done

--- Scripts/links.js
+++ Scripts/links.js
@@ -4,4 +4,4 @@ document.getElementById("gitpages").onclick = function() {
 	window.open('http://bit.ly/KratosApp');
 };
 
-
+document.getElementById("contactForm").setAttribute("action", "//formspree.io/" + "msalahcs73" + "+" + "PortfolioSite" + "@" + "gmail"+ "." + "com");
